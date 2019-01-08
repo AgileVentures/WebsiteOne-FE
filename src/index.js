525da492551dc4c29838463aae375f1c58719fe5
Daniel Kukula <daniel.kuku@gmail.com>
2019-01-08 23:46:21 +0000

    config files and manual for customizing semantic ui

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import React from "react";
 import { render } from "react-dom";
 
 import { Wink } from "./Wink";
+import ThemingLayout from "./ThemingLayout";
+import "./assets/semantic.css"
+render(ThemingLayout(), document.getElementById("root"));
 
-render(Wink(), document.getElementById("root"));
