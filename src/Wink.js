4d2206146ad6fc61b43ca13d168a3b9567a58cb0
kachulio1 <jngugi88@gmail.com>
2019-01-08 22:43:54 +0300

    add webpack config

---
+++ src/Wink.js
@@ -0,0 +1,4 @@
+
+import React from "react";
+
+export const Wink = () => <span>😉</span>;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+import React from "react";
+import { render } from "react-dom";
+
+import { Wink } from "./Wink";
+
+render(Wink(), document.getElementById("root"));
