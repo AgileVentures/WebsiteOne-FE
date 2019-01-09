b191d6a4b0a06c3873d1fe98a25b685fa3318735
Daniel Kukula <daniel.kuku@gmail.com>
2019-01-09 03:52:48 +0000

    remove unused import from index page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 import React from "react";
 import { render } from "react-dom";
 
-import ThemingLayout from "./ThemingLayout";
 import Homepage from "./components/homepage/Homepage";
 import "./assets/semantic.css"
 render(<Homepage/>, document.getElementById("root"));
