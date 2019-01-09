3adede125ae435c9b3ce7e6568f704381c66a919
mattwr18 <mattwr18@gmail.com>
2019-01-09 01:15:52 -0200

    Add UsersList container, test (#7)
    * Add UsersList container, test
    
    fixes #5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-
 import React from "react";
 import { render } from "react-dom";
+import { UsersList } from "./containers/UsersList";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import { Wink } from "./Wink";
-
-render(Wink(), document.getElementById("root"));
+render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/users" component={UsersList} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
