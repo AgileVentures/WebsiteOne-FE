4185df4f74b2bb537baa4cdd390a0a464128acfe
Daniel Kukula <daniel.kuku@gmail.com>
2019-01-09 16:06:11 +0000

    Homepage (#15)
    * add path module
    
    * add build cmd
    
    * add new line
    
    * fix style loader
    
    * config files and manual for customizing semantic ui
    
    * keep only changes in site.variables
    
    * removed unused import
    
    * removed unused import from tests
    
    * add semantic folder to gitignore
    
    * add deleted test
    
    * exclude assets folders from coverage report
    
    * added assets
    
    * clean assets
    
    * add source files
    
    * add images files
    
    * add fixtures files
    
    * fix jest failing on svg import
    
    * rename page title
    
    * render hemepage
    
    * cleaned tests
    
    * remove unused import from index page
    
    * Remove extra space

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import { render } from "react-dom";
 import { UsersList } from "./containers/UsersList";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Homepage from "./components/homepage/Homepage";
+import "./assets/semantic.css"
 
 import "./assets/semantic.css"
 
 render(
   <BrowserRouter>
     <Switch>
+      <Route path="/" exact={true} component={Homepage} />
       <Route path="/users" component={UsersList} />
     </Switch>
   </BrowserRouter>,
