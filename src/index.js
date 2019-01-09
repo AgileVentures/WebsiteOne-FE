6c4ae071926e3ee846031d975a2d80c94ca663d5
Daniel Kukula <daniel.kuku@gmail.com>
2019-01-09 16:06:11 +0000

    Homepage (#15)
    * add path module
    
    * add build cmd
    
    * add new line
    
    * fix style loader
    
    * config files and manual for customizing semantic ui
    
    * keep only changes in site.variables
    
    * removed unused import
    
    * removed unused import from tests
    
    * add semantic folder to gitignore
    
    * add deleted test
    
    * exclude assets folders from coverage report
    
    * added assets
    
    * clean assets
    
    * add source files
    
    * add images files
    
    * add fixtures files
    
    * fix jest failing on svg import
    
    * rename page title
    
    * render hemepage
    
    * cleaned tests
    
    * remove unused import from index page
    
    * Remove extra space

--- src/index.js
+++ src/index.js
@@ -6,14 +6,17 @@ import { Provider } from "react-redux";
 import store from "./store";
 import { Container } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
-
+import Homepage from "./components/homepage/Homepage";
 import "./assets/semantic.css"
+import axios from 'axios';
+axios.defaults.baseURL = 'https://develop.websiteone.agileventures.org/';
 
 render(
   <BrowserRouter>
     <Provider store={store}>
       <Container className="main-content">
         <Switch>
+          <Route path="/" exact={true} component={Homepage} />
           <Route path="/users" component={UsersList} />
         </Switch>
       </Container>
