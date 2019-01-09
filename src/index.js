3bcd300af523e3ca22666fe2f54691c47415979d
Daniel Kukula <daniel.kuku@gmail.com>
2019-01-09 00:05:54 +0000

    removed unused import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 import React from "react";
 import { render } from "react-dom";
 
-import { Wink } from "./Wink";
 import ThemingLayout from "./ThemingLayout";
 import "./assets/semantic.css"
 render(ThemingLayout(), document.getElementById("root"));
