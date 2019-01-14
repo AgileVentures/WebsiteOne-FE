e06280a8273307090be6e4620be46fdd4444ffcf
Matt Rider <mattwr18@gmail.com>
2019-01-14 21:56:12 -0200

    Configure jest to ignore cypress tests

--- jest.config.js
+++ jest.config.js
@@ -15,6 +15,10 @@ module.exports = {
     "!**/src/index.js",
     "!**/cypress/**"
   ],
+  testPathIgnorePatterns: [
+    "<rootDir>/cypress/",
+    "<rootDir>/node_modules/"
+  ],
   "transform": {
     "^.+\\.js$": "babel-jest",
     ".+\\.(css|styl|less|sass|scss)$": "jest-transform-css",
