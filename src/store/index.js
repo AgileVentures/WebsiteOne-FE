20d6e86493cb532a7d11b42a8d180697e2a3d1c3
Matt Rider <mattwr18@gmail.com>
2019-01-15 21:56:23 -0200

    Add redux to projects, pagination

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
 import thunk from 'redux-thunk'
 import users from '../reducers/usersReducer'
+import projects from '../reducers/projectsReducer'
 
 const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose
 const rootReducer = combineReducers({
-  users
+  users,
+  projects
 })
 export default createStore(
   rootReducer,
