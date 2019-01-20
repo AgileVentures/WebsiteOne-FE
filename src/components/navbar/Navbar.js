05a7dce4ec67d77eb279ccad72cc2f031290f1c6
Daniel Kukula <daniel.kuku@gmail.com>
2019-01-20 18:53:21 +0000

    Initial navbar draft

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { Menu, Container, Image } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { withRouter } from "react-router";
+import logo from "../../images/av-logo.svg";
+import "./Navbar.css";
+
+class Navbar extends Component {
+
+  currentPath() {
+    // this.props.location.split("/") returns ["", ""] when on homepage
+    // and ["", "users", "123"] when on /users/123
+    // This function is used to highlight the nav link
+    const pathArray = this.props.location.pathname.split("/");
+    return pathArray[1];
+  }
+
+  render() {
+    const activeItem = this.currentPath();
+
+    return (
+      <Menu stackable borderless inverted as="div" className="navbar">
+        <Container>
+          <Menu.Item header name="" active={activeItem === ""}>
+            <Link to="/">
+              <Image src={logo} avatar />
+              AgileVentures
+            </Link>
+          </Menu.Item>
+
+          <Menu.Menu position="right">
+            <Menu.Item name="users" active={activeItem === "users"}>
+              <Link to="/users">Users</Link>
+            </Menu.Item>
+
+            <Menu.Item
+              name="projects"
+              active={activeItem === "projects"}
+              disabled
+            >
+              Projects
+            </Menu.Item>
+            <Menu.Item name="events" active={activeItem === "events"} disabled>
+              Events
+            </Menu.Item>
+
+            <Menu.Item
+              name="sign-in"
+              active={activeItem === "sign-in"}
+              disabled
+            >
+              Sign-in
+            </Menu.Item>
+          </Menu.Menu>
+        </Container>
+      </Menu>
+    );
+  }
+}
+export default withRouter(Navbar);
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux'
 import store from './store'
 import { Container } from 'semantic-ui-react'
 import Homepage from './components/homepage/Homepage'
+import Navbar from './components/navbar/Navbar'
 import './assets/semantic.css'
 import axios from 'axios'
 axios.defaults.baseURL = 'https://develop.websiteone.agileventures.org/'
@@ -13,6 +14,7 @@ axios.defaults.baseURL = 'https://develop.websiteone.agileventures.org/'
 render(
   <BrowserRouter>
     <Provider store={store}>
+      <Navbar></Navbar>
       <Container className='main-content'>
         <Switch>
           <Route path='/' exact component={Homepage} />
