15aa12e54fb441c48bf929fe4e7b2263814379eb
Matt Rider <mattwr18@gmail.com>
2019-01-21 14:29:59 -0200

    Attempt to execute billing agreement
    currently getting a 404 not found erroq

--- src/containers/PayPalRecurring.js
+++ src/containers/PayPalRecurring.js
@@ -1,34 +1,38 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import paypal from 'paypal-checkout'
-import { PAYPAL_CLIENT_ID_SANDBOX, PAYPAL_CLIENT_ID_PRODUCTION } from 'babel-dotenv'
+import {
+  PAYPAL_CLIENT_ID_SANDBOX,
+  PAYPAL_CLIENT_ID_PRODUCTION,
+  ACCESS_TOKEN
+} from 'babel-dotenv'
+import axios from 'axios'
 const PayPalButton = paypal.Button.driver('react', { React, ReactDOM })
 
 let client = {
   sandbox: PAYPAL_CLIENT_ID_SANDBOX,
   production: PAYPAL_CLIENT_ID_PRODUCTION
 }
+console.log(paypal)
 
 const ENV = process.env.NODE_ENV === 'production' ? 'production' : 'sandbox'
 let billingAgreement = () => {
-  // var links = {}
-
   return paypal.rest.billingAgreement
     .create(ENV, client, {
       name: 'Premium Membership',
-      description: 'Monthly agreement with a regular monthly payment definition and 1-week trial payment definition.',
+      description:
+        'Monthly agreement with a regular monthly payment definition and 1-week trial payment definition.',
       start_date: '2019-01-20T09:13:49Z',
-      plan:
-      {
+      plan: {
         type: 'MERCHANT_INITIATED_BILLING',
         id: 'P-98S68313UK7404721NSNR7LI'
         //  type: 'RECURRING_PAYMENTS'
       },
-      payer:
-      {
+      payer: {
         payment_method: 'paypal'
       }
-    }).then(billingAgreement => {
+    })
+    .then(billingAgreement => {
       console.log(JSON.stringify(billingAgreement))
       console.log('Billing Agreement Created Successfully')
       return billingAgreement
@@ -42,17 +46,13 @@ let billingAgreement = () => {
 let onAuthorize = (data, actions) => {
   console.log('data', data)
   console.log('actions', actions)
-  // window.location.replace(data.returnUrl)
-  // actions.payment.execute()
-  // paypal.billingAgreement.execute(data.paymentToken, {}, (error, billingAgreement) => {
-  //   if (error) {
-  //     console.error(JSON.stringify(error))
-  //     throw error
-  //   } else {
-  //     console.log(JSON.stringify(billingAgreement))
-  //     console.log('Billing Agreement Created Successfully')
-  //   }
-  // })
+  axios.post({
+    authorization: `Basic ${ACCESS_TOKEN}`,
+    url: `https://api.sandbox.paypal.com/v1/payments/billing-agreements/${data.paymentToken}/agreement-execute`
+  }).catch(error => {
+    console.error(JSON.stringify(error))
+    throw error
+  })
 }
 
 class PayPalRecurring extends Component {
