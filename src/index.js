d02ac928c21c0057b5d0943edb53dd913c08e0a9
Matt Rider <mattwr18@gmail.com>
2019-01-22 12:04:28 -0200

    Try using paypal-rest-sdk

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import store from './store'
 import { Container } from 'semantic-ui-react'
 import Homepage from './components/homepage/Homepage'
 import SignUp from './containers/SignUp'
-import PayPalRecurring from './containers/PayPalRecurring'
+import PayPalRecurringSDK from './containers/PayPalRecurringSDK'
 import './assets/semantic.css'
 import axios from 'axios'
-axios.defaults.baseURL = 'https://develop.websiteone.agileventures.org/'
+// axios.defaults.baseURL = 'https://develop.websiteone.agileventures.org/'
 
 render(
   <BrowserRouter>
@@ -20,7 +20,7 @@ render(
           <Route path='/' exact component={Homepage} />
           <Route path='/users' component={UsersList} />
           <Route path='/signup' component={SignUp} />
-          <Route path='/subscriptions' component={PayPalRecurring} />
+          <Route path='/subscriptions' component={PayPalRecurringSDK} />
         </Switch>
       </Container>
     </Provider>
