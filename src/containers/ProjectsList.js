a3b107ac8e436c3ced47370e1097c7433fc593d7
mattwr18 <mattwr18@gmail.com>
2019-01-22 12:03:07 -0200

    Add ProjectList container (#41)

---
+++ src/containers/ProjectsList.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import { Header } from 'semantic-ui-react'
+
+export class ProjectsList extends Component {
+  render () {
+    return <Header>List of Projects</Header>
+  }
+}
