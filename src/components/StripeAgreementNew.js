60574d811b9100229d592786c3c35c74e774cbcc
Daksh Miglani <hello@dak.sh>
2019-02-15 20:28:19 +0530

    as matt requested, send stripe req

--- src/components/StripeAgreementNew.js
+++ src/components/StripeAgreementNew.js
@@ -2,13 +2,32 @@ import React from 'react'
 import { Segment, Header } from 'semantic-ui-react'
 import StripeCheckout from 'react-stripe-checkout'
 import logo from '../images/av-logo.svg'
+import axios from '../helpers/http'
 
 export default function ({ plan, stripeKey }) {
-  function onToken (token) {
+  async function onToken (token) {
     // send request to website one backend.
-    console.log(token)
-  }
+    const { id, email } = token
+    const formKV = {
+      stripeEmail: email,
+      stripeToken: id
+    }
 
+    try {
+      const response = await axios.post(
+        `/subscriptions?plan=${plan.name}`,
+        formKV,
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        }
+      )
+      console.log(response)
+    } catch (e) {
+      // handle error.
+    }
+  }
   return (
     <Segment padded='very' className='paypal-section' raised>
       <Header as='h5'>Get Premium Mob via Credit/Debit Card:</Header>
