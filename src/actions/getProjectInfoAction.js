d85b6255e2a69afc307305f994feb7fe49d64e52
Matt Rider <mattwr18@gmail.com>
2019-02-17 01:16:30 -0300

    Create basic project's info page

--- src/actions/getProjectInfoAction.js
+++ src/actions/getProjectInfoAction.js
@@ -1,9 +1,21 @@
 import axios from 'axios'
 import { GET_PROJECT_INFO } from '../types'
 
-export let fetchProjectInfo = id => dispatch => {
-  return axios.get(`api/v1/projects/${id}`).then(response => {
-    let { project } = response.data
+export let fetchProjectInfo = slug => dispatch => {
+  return axios.get(`api/v1/projects/${slug}`).then(response => {
+    let {
+      project,
+      projectManager,
+      sourceRepositories,
+      members,
+      membersGravatarUrl,
+      videos
+    } = response.data
+    project.projectManager = projectManager
+    project.sourceRepositories = sourceRepositories
+    project.members = members
+    project.membersGravatarUrl = membersGravatarUrl
+    project.videos = videos
     dispatch({ type: GET_PROJECT_INFO, payload: project })
   })
 }
---
+++ src/components/ProjectSummary.js
@@ -0,0 +1,139 @@
+import React, { Fragment } from 'react'
+import { Header, Image, Grid, Card, Icon, Segment, Embed } from 'semantic-ui-react'
+import { RingLoader } from 'react-spinners'
+import { Link } from 'react-router-dom'
+import '../assets/ProjectSummary.css'
+
+const ProjectSummary = props => {
+  console.log(props)
+  let { project } = props
+  if (project) {
+    return (
+      <Grid columns={2} stackable>
+        <Grid.Column width={12}>
+          <Fragment>
+            {project.image_url ? (
+              <Image
+                src={project.image_url}
+                floated='left'
+                className='project-info-image'
+              />
+            ) : null}
+            <Header as='h1'>{project.title}</Header>
+            <Segment raised>
+              <Header as='h5'>{project.description}</Header>
+            </Segment>
+            <Grid columns={2} stackable>
+              <Grid.Row>
+                <Grid.Column width={8}>
+                  <Card fluid raised className='project-info-card'>
+                    <Card.Content>
+                      <Card.Header>Connected on</Card.Header>
+                      <Card.Description className='project-info-card-description'>
+                        {project.sourceRepositories.map(repo => {
+                          let array = repo.url.split('/')
+                          let repoName = array[array.length - 1] || array[array.length - 2]
+                          return (
+                            <Fragment key={repo.id}>
+                              <Grid>
+                                <Grid.Row>
+                                  <Icon name='github alternate' size='large' />
+                                  <a href={repo.url}>{repoName}</a>&nbsp; on GitHub
+                                </Grid.Row>
+                              </Grid>
+                            </Fragment>
+                          )
+                        }) || null}
+                        {project.pivotaltracker_url ? (
+                          <Fragment key={project.id}>
+                            <Grid>
+                              <Grid.Row>
+                                <Icon name='bug' size='large' />
+                                <a href={project.pivotaltracker_url}>
+                                  {project.title}
+                                </a>
+                            &nbsp; on IssueTracker
+                              </Grid.Row>
+                            </Grid>
+                          </Fragment>
+                        ) : null}
+                        {
+                          <Fragment key={project.id}>
+                            <Grid>
+                              <Grid.Row>
+                                <Icon name='slack hash' size='large' />
+                                <a
+                                  href={`https://agileventures.slack.com/app_redirect?channel=${
+                                    project.slack_channel_name
+                                  }`}
+                                >
+                                  {project.title}
+                                </a>
+                            &nbsp; on Slack
+                              </Grid.Row>
+                            </Grid>
+                          </Fragment>
+                        }
+                      </Card.Description>
+                    </Card.Content>
+                  </Card>
+                </Grid.Column>
+                <Grid.Column>
+                  <Card fluid className='project-info-card'>
+                    <Card.Content>
+                      <Card.Header>Members</Card.Header>
+                      <Card.Description className='project-info-card-description'>
+                        {project.members.map(member => {
+                          return (
+                            <Fragment key={member.id}>
+                              <Grid>
+                                <Grid.Row>
+                                  <Image
+                                    src={
+                                      project.membersGravatarUrl[`${member.slug}`]
+                                    }
+                                  />&nbsp;
+                                  <Link to={`/users/${member.id}`}>
+                                    {member.first_name ? `${member.first_name} ${member.last_name}`
+                                      : member.slug}
+                                  </Link>
+                                </Grid.Row>
+                              </Grid>
+                            </Fragment>
+                          )
+                        })}
+                      </Card.Description>
+                    </Card.Content>
+                  </Card>
+                </Grid.Column>
+                <Grid.Column></Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Fragment>
+        </Grid.Column>
+        <Grid.Column width={4} className='project-info-videos'>
+          {project.videos
+            .filter(video => video.yt_video_id !== null)
+            .map(video => (
+              <Fragment key={video.id}>
+                <Embed
+                  id={video.yt_video_id}
+                  placeholder={
+                    'https://img.youtube.com/vi/' +
+                        video.yt_video_id +
+                        '/3.jpg'
+                  }
+                  source='youtube'
+                />
+                <p>{video.title}</p>
+              </Fragment>
+            ))}
+        </Grid.Column>
+      </Grid>
+    )
+  } else {
+    return <RingLoader sizeUnit={'px'} size={200} color={'#34495E'} />
+  }
+}
+
+export default ProjectSummary
