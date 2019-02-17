cb7f17f017cdfe50b37412eee3aab08330f12b96
Matt Rider <mattwr18@gmail.com>
2019-02-17 16:00:59 -0300

    Write component tests for ProjectInfo, ProjectSummary

--- src/fixtures/projectInfo.js
+++ src/fixtures/projectInfo.js
@@ -8,11 +8,42 @@ export default {
   user_id: 1,
   slug: 'websiteone',
   github_url: 'https://github.com/AgileVentures/WebsiteOne',
-  pivotaltracker_url: null,
+  pivotaltracker_url: 'https://waffle.io/AgileVentures/WebsiteOne',
   pitch: null,
   commit_count: 190,
   image_url: null,
   last_github_update: null,
   slack_channel_name: null,
-  tag_list: []
+  tag_list: [],
+  sourceRepositories: [{
+    id: 77,
+    url: 'https://github.com/AgileVentures/WebsiteOne',
+    project_id: 2,
+    created_at: '2017-09-27T15:01:52.695Z',
+    updated_at: '2017-09-27T15:01:52.695Z'
+  }],
+  members: [{
+    id: 2,
+    slug: 'mattwr18'
+  }],
+  membersGravatarUrl: [{
+    'mattwr18': 'https://www.gravatar.com/avatar/9249736dae1898d537770886061c06f9?s=32&d=retro'
+  }],
+  videos: [{
+    id: 6728,
+    event_id: 2676,
+    title: 'WebsiteOne Planning - Monday, 12th Mar at 04:30pm (UTC)',
+    hangout_url: 'https://hangouts.google.com/hangouts/_/ytl/XOYi7nc3GUnbaxQU_J3ToWVE56FIoFztizc5nUfT8sg=',
+    created_at: '2018-03-12T16:30:55.399Z',
+    updated_at: '2018-03-12T16:31:05.707Z',
+    uid: '300-18c2c4db-9aac-428d-8b6e-18c61e8d44e2',
+    category: 'Scrum',
+    project_id: 2,
+    user_id: 3,
+    yt_video_id: '-a--mSOR4WA',
+    participants: null,
+    hoa_status: null,
+    url_set_directly: true,
+    youtube_tweet_sent: null
+  }]
 }
---
+++ src/tests/components/ProjectSummary.test.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import ProjectSummary from '../../components/ProjectSummary'
+import { StaticRouter } from 'react-router'
+import project from '../../fixtures/projectInfo'
+
+describe('ProjectSummary', () => {
+  let wrapper
+  const context = {}
+  const props = {
+    project
+  }
+  beforeEach(() => {
+    wrapper = mount(
+      <StaticRouter context={context}>
+        <ProjectSummary {...props} />
+      </StaticRouter>
+    )
+  })
+
+  it('displays a link to GitHub repo', () => {
+    project.sourceRepositories[0].url = 'https://github.com/AgileVentures/WebsiteOne'
+    let linkToRepo = wrapper.find('a').filterWhere(item => {
+      return item.prop('href') === project.sourceRepositories[0].url
+    })
+
+    expect(linkToRepo.text()).toEqual('WebsiteOne')
+  })
+
+  it('handles repo urls with ending in forward slashes', () => {
+    project.sourceRepositories[0].url = 'https://github.com/AgileVentures/WebsiteOne/'
+    wrapper = mount(
+      <StaticRouter context={context}>
+        <ProjectSummary {...props} />
+      </StaticRouter>
+    )
+    let linkToRepo = wrapper.find('a').filterWhere(item => {
+      return item.prop('href') === project.sourceRepositories[0].url
+    })
+    expect(linkToRepo.text()).toEqual('WebsiteOne')
+  })
+
+  it('displays a spinner when no project is in the props', () => {
+    wrapper = mount(
+      <StaticRouter context={context}>
+        <ProjectSummary />
+      </StaticRouter>
+    )
+    expect(wrapper.find('Loader').props().loading).toBe(true)
+  })
+
+  it("displays a project's image if there is one", () => {
+    project.image_url = 'http://i.imgur.com/23ePvyN.jpg'
+    wrapper = mount(
+      <StaticRouter context={context}>
+        <ProjectSummary {...props} />
+      </StaticRouter>
+    )
+    let projectsImageUrl = wrapper.find('Image').filterWhere(item => {
+      return item.hasClass('project-info-image')
+    })
+    expect(projectsImageUrl.props().src).toEqual(project.image_url)
+  })
+
+  it("displays a member's name if there is one", () => {
+    project.members[0].first_name = 'Matt'
+    project.members[0].last_name = 'Rider'
+    wrapper = mount(
+      <StaticRouter context={context}>
+        <ProjectSummary {...props} />
+      </StaticRouter>
+    )
+    let linkToMember = wrapper.find('Link').filterWhere(item => {
+      return item.prop('to') === '/users/2'
+    })
+    expect(linkToMember.text()).toEqual('Matt Rider')
+  })
+})
