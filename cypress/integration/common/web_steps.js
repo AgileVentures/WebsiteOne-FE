4b00d7954a8d809941fd1923473feee64589654b
Matt Rider <mattwr18@gmail.com>
2019-02-20 22:34:41 -0300

    Get tests passing with stubbing/fixtures

--- cypress/integration/common/web_steps.js
+++ cypress/integration/common/web_steps.js
@@ -1,10 +1,10 @@
 import { Then, Given, When } from 'cypress-cucumber-preprocessor/steps'
 
-Then(`I should see {string}`, (textToFind) => {
+Then(`I should see {string}`, textToFind => {
   cy.contains(textToFind)
 })
 
-Given(`I am at the {string} page`, (pageName) => {
+Given(`I am at the {string} page`, pageName => {
   if (pageName === 'Home') {
     cy.visit('/')
   } else {
@@ -12,25 +12,54 @@ Given(`I am at the {string} page`, (pageName) => {
   }
 })
 
+Given('I am at the projects page', () => {
+  cy.server()
+  cy.fixture('projects').then(projects => {
+    cy.route(
+      /https:\/\/develop\.websiteone\.agileventures\.org\/api\/v1\/projects/,
+      projects
+    ).as('getProjects')
+    cy.visit('/projects')
+    cy.window()
+      .its('store')
+      .invoke('dispatch', { type: 'GET_PROJECTS', payload: projects })
+  })
+  cy.wait('@getProjects')
+})
+
 When('I click on the LocalSupport project', () => {
-  // cy.server()
-  // cy.fixture('projects').then(projects => {
-  //   cy.route(/https:\/\/develop\.websiteone\.agileventures\.org\/api\/v1\/projects/, projects).as('getProjects')
-  // })
-  // cy.wait('@getProjects')
-  cy.get('div', { timeout: 10000 }).contains('LocalSupport').click({ force: true })
+  cy.server()
+  cy.fixture('project').then(project => {
+    cy.route(/https:\/\/develop\.websiteone\.agileventures\.org\/api\/v1\/projects\/localsupport/, project).as('getProject')
+    cy.get('div')
+      .contains('LocalSupport')
+      .click({ force: true })
+    cy.window()
+      .its('store')
+      .invoke('dispatch', { type: 'GET_PROJECT_INFO', payload: project })
+  })
+  cy.wait('@getProject')
+  cy.reload()
 })
 
 Then("I should be on the LocalSupport Project's info page", () => {
   cy.get('h1').contains('LocalSupport')
 })
 
-Then('I should see the tracker link, the github repositories links, a small description, a members list, and a video wall', () => {
-  cy.get("a[href='https://www.pivotaltracker.com/n/projects/742821']")
-    .get("a[href='https://github.com/AgileVentures/LocalSupport/']")
-    .get('div.segment > h5').contains("VAH's Local Support site is at  www.harrowcn.org.uk")
-    .get('div').contains('Members').get('div.row > img').should('have.length', 5)
-    .get('.project-info-videos').within(() => {
-      cy.get('div.embed').should('have.length', 5)
-    })
-})
+Then(
+  'I should see the tracker link, the github repositories links, a small description, a members list, and a video wall',
+  () => {
+    cy.get("a[href='https://www.pivotaltracker.com/n/projects/742821']")
+      .get("a[href='https://github.com/AgileVentures/LocalSupport/']")
+      .get('div.segment > h5')
+      .contains("VAH's Local Support site is at  www.harrowcn.org.uk")
+      .get('div')
+      .contains('Members')
+      .get('div.row > img')
+      .should('have.length', 5)
+      .get('.project-info-videos')
+      .within(() => {
+        cy.get('div.embed').should('have.length', 5)
+      })
+  }
+)
--- cypress/integration/projectsInfo.feature
+++ cypress/integration/projectsInfo.feature
@@ -1,7 +1,7 @@
 Feature: Project's info page
 
   Scenario: Access a Project's info page
-    Given I am at the "projects" page
+    Given I am at the projects page
     When I click on the LocalSupport project
     Then I should be on the LocalSupport Project's info page
     And I should see the tracker link, the github repositories links, a small description, a members list, and a video wall
