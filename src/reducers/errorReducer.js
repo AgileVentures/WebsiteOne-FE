ff6ef13decce0c66c3ec850ff127496370e0aefb
Matt Rider <mattwr18@gmail.com>
2019-02-25 08:09:51 -0300

    Write tests, add new failure type

--- src/reducers/errorReducer.js
+++ src/reducers/errorReducer.js
@@ -1,7 +1,8 @@
 import {
   FETCH_PROJECTS_FAILURE,
   CREATE_BILLING_AGREEMENT_FAILURE,
-  EXECUTE_BILLING_AGREEMENT_FAILURE
+  EXECUTE_BILLING_AGREEMENT_FAILURE,
+  CREATE_STRIPE_SUBSCRIPTION_FAILURE
 } from '../types'
 import initialState from './initialState'
 
@@ -12,6 +13,8 @@ const errorReducer = (state = initialState.error, action) => {
     case CREATE_BILLING_AGREEMENT_FAILURE:
       return [action.message]
     case EXECUTE_BILLING_AGREEMENT_FAILURE:
+      return [action.message]
+    case CREATE_STRIPE_SUBSCRIPTION_FAILURE:
       return [action.message]
     default:
       return state
