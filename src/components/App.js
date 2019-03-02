9df17f7aa610a2115320ce86bc98d273331fdd91
aonomike <aonomike@gmail.com>
2019-03-02 16:58:29 +0300

    Add routing and links to the page

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,7 @@ import MembershipPlansPage from '../containers/MembershipPlansPage'
 import PremiumMembershipPage from '../containers/PremiumMembershipPage'
 import PremiumMobMembershipPage from '../containers/PremiumMobMembershipPage'
 import PremiumF2FMembershipPage from '../containers/PremiumF2FMembershipPage'
+import GettingStartedPage from '../containers/GettingStartedPage'
 import EventsList from '../containers/EventsList'
 import EventInfo from '../containers/EventInfo'
 import { withCookies } from 'react-cookie'
@@ -73,6 +74,7 @@ class App extends Component {
           <Route path='/premium-f2f' component={PremiumF2FMembershipPage} />
           <Route exact path='/events' component={EventsList} />
           <Route path='/events/:slug' component={EventInfo} />
+          <Route path='/getting-started' component={GettingStartedPage} />
         </Switch>
       </Fragment>
     )
