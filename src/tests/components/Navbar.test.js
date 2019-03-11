2b59ad634ba826e498fd88a3b9886e805d4e37a1
nataliya <rabbiyan786@yahoo.com>
2019-03-11 10:48:18 +0000

    resolved confict

--- src/tests/components/Navbar.test.js
+++ src/tests/components/Navbar.test.js
@@ -13,19 +13,6 @@ describe('Navbar', () => {
       expect(wrapper.find('Navbar').length).toEqual(1)
     })
 
-<<<<<<< HEAD
-    it('renders 8 MenuItem elements', () => {
-      const wrapper = homepage(props)
-      expect(wrapper.find('MenuItem').length).toEqual(8)
-    })
-
-    it('renders 8 Link elements', () => {
-      const wrapper = homepage(props)
-      expect(wrapper.find('Link').length).toEqual(8)
-    })
-
-=======
->>>>>>> b2e0e8c14d59d1a10bce0919758bf06f0f59fcd8
     it('renders 1 active Link element', () => {
       const wrapper = homepage(props)
       expect(wrapper.find('.active').find('Link').length).toEqual(1)
