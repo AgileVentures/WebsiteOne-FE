da6c58add7b0ff5714ec3d7d719a2e6946c88ae7
Matt Rider <mattwr18@gmail.com>
2019-03-14 19:19:24 -0300

    Add handleChange functionality, add more input fields

--- cypress/integration/create_events.feature
+++ cypress/integration/create_events.feature
@@ -1,7 +1,7 @@
-Feature: Create new project
+Feature: Create new event
   Background:
     Given the server is running
 
-  Scenario: Creating a new project
+  Scenario: Creating a new event
     Given I am logged in
     Then I should be able to create new events
--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,47 +1,147 @@
-import React from 'react'
-import { Button, Form } from 'semantic-ui-react'
+import React, { Fragment } from 'react'
+import { Button, Form, Grid } from 'semantic-ui-react'
 import DatePicker from 'react-datepicker'
 import momentTZ from 'moment-timezone'
+import { Link } from 'react-router-dom'
+import moment from 'moment'
 
 import 'react-datepicker/dist/react-datepicker.css'
 
 const EventForm = props => {
+  console.log(props)
   const categoryOptions = [
-    { key: 'pp', text: 'PairProgramming' },
-    { key: 's', text: 'Scrum' },
-    { key: 'cm', text: 'ClientMeeting' }
+    { key: 'pp', text: 'PairProgramming', value: 'PairProgramming' },
+    { key: 'scrum', text: 'Scrum', value: 'Scrum' },
+    { key: 'client', text: 'ClientMeeting', value: 'ClientMeeting' }
   ]
-  const forOptions = [
-    { key: 'a', text: 'All' },
-    { key: 'pmm', text: 'Premium Mob Members' }
+  const eventForOptions = [
+    { key: 'all', text: 'All', value: 'All' },
+    { key: 'premium', text: 'Premium Mob Members', value: 'Premium Mob Members' }
   ]
-
   const projectOptions = props.projects.map(project => {
-    return { key: project.id, text: project.title }
+    return { key: project.id, text: project.title, value: project.id }
   })
-  const timeZonesList = momentTZ.tz.names().map(timeZone => {
-    return { key: timeZone, text: timeZone }
+  const timeZonesOptions = momentTZ.tz.names().map(timeZone => {
+    return { key: timeZone, text: timeZone, value: timeZone }
   })
+  const repeatsOptions = [
+    { key: 'never', text: 'never', value: 'never' },
+    { key: 'weekly', text: 'weekly', value: 'weekly' },
+    { key: 'biweekly', text: 'biweekly', value: 'biweekly' }
+  ]
+  const daysOfTheWeek = moment.weekdays().map(weekday => {
+    return { key: weekday, text: weekday, value: weekday }
+  })
+  const repeatEndsOptions = [
+    { key: 'on', text: 'on', value: 'on' },
+    { key: 'never', text: 'never', value: 'never' }
+  ]
   return (
-    <Form onSubmit={props.onSubmit}>
-      <Form.Input label='Name' name='name' />
-      <Form.Select label='Category' options={categoryOptions} placeholder={categoryOptions[0].text} />
-      <Form.Select label='For' options={forOptions} placeholder={forOptions[0].text} />
-      <Form.Select label='Project' options={projectOptions} placeholder={''} />
-      <Form.TextArea label='Description' name='description' />
-      <p>Start Date</p>
-      <DatePicker selected={props.startDate} onChange={props.handleChange} />
-      <p>Start Time</p>
-      <DatePicker
-        selected={props.startDate}
+    <Form onSubmit={props.handleSubmit}>
+      <Form.Input
+        label='Name'
+        name='name'
+        value={props.name}
         onChange={props.handleChange}
-        showTimeSelect
-        showTimeSelectOnly
-        timeIntervals={15}
-        dateFormat='h:mm aa'
-        timeCaption='Time'
       />
-      <Form.Select options={timeZonesList} placeholder={momentTZ.tz.guess()} />
+      <Form.Select
+        label='Category'
+        name='category'
+        options={categoryOptions}
+        placeholder={categoryOptions[0].text}
+        value={categoryOptions.value}
+        onChange={props.handleChange}
+      />
+      <Form.Select
+        label='For'
+        name='eventFor'
+        options={eventForOptions}
+        placeholder={eventForOptions[0].text}
+        value={eventForOptions.value}
+        onChange={props.handleChange}
+      />
+      <Form.Select
+        label='Project'
+        name='project'
+        options={projectOptions}
+        placeholder={''}
+        value={projectOptions.value}
+        onChange={props.handleChange}
+        search
+      />
+      <Form.TextArea
+        label='Description'
+        name='description'
+        value={props.description}
+        onChange={props.handleChange}
+      />
+      <Grid columns={2}>
+        <Grid.Row>
+          <Grid.Column>
+            <label>Start Date</label><br />
+            <DatePicker
+              selected={props.startDate}
+              onChange={props.handleDateChange}
+            />
+          </Grid.Column>
+          <Grid.Column>
+            <label>Start Time</label><br />
+            <DatePicker
+              selected={props.startDate}
+              onChange={props.handleDateChange}
+              showTimeSelect
+              showTimeSelectOnly
+              timeIntervals={15}
+              dateFormat='h:mm aa'
+              timeCaption='Time'
+            />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      <Form.Select
+        options={timeZonesOptions}
+        placeholder={momentTZ.tz.guess()}
+        search
+      />
+      <Form.Field label='Duration' control='input' type='number' />
+      <Form.Select
+        label='Repeats'
+        name='repeats'
+        options={repeatsOptions}
+        placeholder={repeatsOptions[0].text}
+        value={repeatsOptions.value}
+        onChange={props.handleChange}
+      />
+      {props.repeats && props.repeats !== 'never' ? (
+        <Fragment>
+          <Form.Select
+            label='Each'
+            name='weekdays'
+            options={daysOfTheWeek}
+            multiple
+            onChange={props.handleChange}
+          />
+          <Form.Select
+            label='Repeat ends'
+            name='repeatEnds'
+            options={repeatEndsOptions}
+            placeholder={'on'}
+            value={props.repeatsEnd}
+            onChange={props.handleChange}
+          />
+        </Fragment>
+      ) : null}
+      {props.repeats && props.repeatEnds === 'on'
+        ? <Fragment>
+          <label>End Date</label><br />
+          <DatePicker selected={props.endDate}
+            onChange={props.handleDateChange}
+          />
+        </Fragment>
+        : null}
+      <Link to={'/events'}>
+        <Button>Cancel</Button>
+      </Link>
       <Button type='submit'>Save</Button>
     </Form>
   )
