f4b7cdb5edb5837aa2f255e71b3b4169f6efb3ea
abdellani <abdellani@gmail.com>
2019-03-25 16:35:49 +0100

    Add New Event button

--- src/containers/EventsList.js
+++ src/containers/EventsList.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { fetchEvents } from '../actions/getEventsAction'
 import { setLastLocation } from '../actions/setLastLocationAction'
-import { Container, Header } from 'semantic-ui-react'
+import { Container, Header, Grid, Popup, Icon, Button } from 'semantic-ui-react'
 import BigCalendar from 'react-big-calendar'
 import moment from 'moment'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
@@ -25,7 +25,29 @@ export class EventsList extends Component {
     return (
       <Fragment>
         <Container>
-          <Header as='h1'>AgileVentures Events</Header>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column width={14}>
+                <Header as='h1'>
+                 AgileVentures Events
+                </Header>
+              </Grid.Column>
+              <Grid.Column width={1}>
+                <a href='events/new'>
+                  <Popup
+                    position='right center'
+                    trigger={
+                      <Button basic style={{ marginTop: '25px' }}>
+                        <Icon name='plus' id='new-event-icon' />
+                      </Button>
+                    }
+                    content='New Event!'
+                  />
+                </a>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+
           <BigCalendar
             localizer={localizer}
             events={events}
