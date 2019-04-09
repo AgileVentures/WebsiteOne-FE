a7064a8b34c546a8bd4cde594f1253b053cf8044
aonomike <aonomike@gmail.com>
2019-04-10 01:49:46 +0300

    Rename projectCreateReducer to createProjectReducer, add tests around create project reducer

---
+++ src/reducers/createProjectReducer.js
@@ -0,0 +1,12 @@
+import { CREATE_PROJECT } from '../types'
+import initialState from './initialState'
+const createProjectReducer = (state = initialState.project, action) => {
+  switch (action.type) {
+    case CREATE_PROJECT:
+      return { ...action.payload }
+    default:
+      return state
+  }
+}
+
+export default createProjectReducer
