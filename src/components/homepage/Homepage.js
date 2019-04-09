7e9bd0cc4a6b32712e3ef1408bcfb06d711c5049
Infinical <inficalvin@gmail.com>
2019-04-09 12:26:15 +0300

    Restyle homepage

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import React, { Component, Fragment } from 'react'
-import { Grid } from 'semantic-ui-react'
-import HomepageModal from './HomepageModal'
-import modals from './modals'
-=======
 import React, { Component } from 'react'
 import Modal from './HomepageModal'
 import modalContent from './modals'
 
->>>>>>> c6a0e7d06a2f617b799a0e4acf72468112b9b2c9
 import './Homepage.css'
 
 export class Homepage extends Component {
