265c1c0fa0860f1fdd32950c3ce183a741f02a58
Matt Rider <mattwr18@gmail.com>
2019-04-18 18:13:30 -0300

    Fix styling

--- src/components/homepage/HomepageModal.js
+++ src/components/homepage/HomepageModal.js
@@ -1,18 +1,22 @@
 import React from 'react'
+import { Container, Grid, Header } from 'semantic-ui-react'
 import './HomepageModal.scss'
 
 export default ({ content }) => (
-  <div className='wrap'>
-    <div className='card'>
-      <div className='card-pic-wrap'>
-        <img src={content.image} alt={content.imageAltText} />
-      </div>
-      <div class='card-content'>
-        <h3>{content.title}</h3>
-        <p>
-          {content.modalText}
-        </p>
-      </div>
-    </div>
-  </div>
+  <Container>
+    <Grid columns={3} id='landing-page-cards' stackable>
+      <Grid.Row>
+        <Grid.Column width={6} className='card-pic-wrap'>
+          <img src={content.image} alt={content.imageAltText} />
+        </Grid.Column>
+        <Grid.Column width={2} />
+        <Grid.Column width={6} className='card-content'>
+          <Header as='h2'>{content.title}</Header>
+          <p>
+            {content.modalText}
+          </p>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  </Container>
 )
