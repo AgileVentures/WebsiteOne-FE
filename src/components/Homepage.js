b28d4fd4fb38c994f7b74550e682842e32b47a2a
mattwr18 <mattwr18@gmail.com>
2019-04-19 13:34:48 -0300

    landing page (#196)
    * Restyling the homepage
    * Get background image working, remove footer

---
+++ src/components/Homepage.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { Grid, Container } from 'semantic-ui-react'
+import Modal from './HomepageModal'
+import modalContent from '../helpers/modals'
+
+import '../assets/Homepage.css'
+
+export class Homepage extends Component {
+  modals = modalContent.map(item => <Modal key={item.reactId} content={item} />);
+  render () {
+    return (
+      <div className='landing-page-background'>
+        <Container>
+          <Grid columns={2} stackable>
+            <Grid.Column width={12}>
+              {this.modals[0]}
+            </Grid.Column>
+            <Grid.Column width={12} floated='right'>
+              {this.modals[1]}
+            </Grid.Column>
+            <Grid.Column width={12}>
+              {this.modals[2]}
+            </Grid.Column>
+            <Grid.Column width={12} floated='right'>
+              {this.modals[3]}
+            </Grid.Column>
+            <Grid.Column width={12}>
+              {this.modals[4]}
+            </Grid.Column>
+          </Grid>
+        </Container>
+      </div>
+    )
+  }
+}
+
+export default Homepage
