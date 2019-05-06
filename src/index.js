f38026c197a8a2e6dbb9adef6e5ce870035f230a
Infinical <inficalvin@gmail.com>
2019-05-06 19:02:38 +0300

    Add a favicon

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import { CookiesProvider } from 'react-cookie'
-import { Provider } from 'react-redux'
-import store from './store'
-import App from './components/App'
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { CookiesProvider } from "react-cookie";
+import { Provider } from "react-redux";
+import store from "./store";
+import App from "./components/App";
+import Favicon from "react-favicon";
 
-import './assets/semantic.css'
+import "./assets/semantic.css";
 
 render(
   <CookiesProvider>
+    <Favicon url="https://www.agileventures.org/favicon.ico?v=2" />
     <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
-  </CookiesProvider>
-  ,
-  document.getElementById('root')
-)
+  </CookiesProvider>,
+  document.getElementById("root")
+);
 
 if (window.Cypress) {
-  window.store = store
+  window.store = store;
 }
