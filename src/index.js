85fdc4ae784c4475bdaf2b877c1fe20acf0aafb2
Infinical <inficalvin@gmail.com>
2019-05-07 06:23:06 +0300

    Use js standard style

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import React from "react";
-import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { CookiesProvider } from "react-cookie";
-import { Provider } from "react-redux";
-import store from "./store";
-import App from "./components/App";
-import Favicon from "react-favicon";
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { CookiesProvider } from 'react-cookie'
+import { Provider } from 'react-redux'
+import store from './store'
+import App from './components/App'
+import Favicon from 'react-favicon'
 
-import "./assets/semantic.css";
+import './assets/semantic.css'
 
 render(
   <CookiesProvider>
-    <Favicon url="https://www.agileventures.org/favicon.ico?v=2" />
+    <Favicon url='https://www.agileventures.org/favicon.ico?v=2' />
     <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
   </CookiesProvider>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
 
 if (window.Cypress) {
-  window.store = store;
+  window.store = store
 }
