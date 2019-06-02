3f3d0c796a778297a659d660ef5807f299e52549
Piotr Brych <pbrych@gmail.com>
2019-06-02 13:09:13 +0200

    Add spec for postResetPasswordAction

---
+++ src/tests/actions/postResetPasswordAction.test.js
@@ -0,0 +1,37 @@
+import moxios from 'moxios'
+import thunk from 'redux-thunk'
+import configureMockStore from 'redux-mock-store'
+import { postResetPassword } from '../../actions/postResetPasswordAction'
+import { POST_RESET_PASSWORD } from '../../types'
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+let store
+
+describe('postLogInInfo action', () => {
+  beforeEach(() => {
+    moxios.install()
+    store = mockStore({})
+  })
+
+  afterEach(() => {
+    moxios.uninstall()
+  })
+
+  it('posts login info to an external api', () => {
+    const expectedActions = [{ type: POST_RESET_PASSWORD, payload: true }]
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent()
+      request.resolve({})
+    })
+
+    return store
+      .dispatch(
+        postResetPassword({ email: 'example@example.com' })
+      )
+      .then(() => {
+        expect(store.getActions()).toEqual(expectedActions)
+      })
+  })
+})
