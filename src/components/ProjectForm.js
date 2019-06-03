0b04d6eb20c831c829c7f8bda7fdd51cdebb4eaa
analogy47 <actweed47@gmail.com>
2019-06-02 23:07:10 -0400

    create two columns for fields

--- src/components/ProjectForm.js
+++ src/components/ProjectForm.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import { Button, Form } from 'semantic-ui-react'
-import { InputField, SelectField, TextAreaField } from 'react-semantic-redux-form'
+import { Button, Form, Grid } from 'semantic-ui-react'
+import {
+  InputField,
+  SelectField,
+  TextAreaField
+} from 'react-semantic-redux-form'
 import { Field, FieldArray, reduxForm } from 'redux-form'
 import FieldGroup from './FieldGroup'
 import { validateProjectForm } from '../helpers/validators'
-// TODO
+
 export const ProjectForm = props => {
   const { handleSubmit, submitting } = props
   const options = [
@@ -14,44 +18,57 @@ export const ProjectForm = props => {
   ]
   return (
     <Form onSubmit={handleSubmit}>
-      <Field name='title' component={InputField}
-        label='Title'
-        placeholder='Title'
-      />
-      <Field type='url'
-        name='image_url' component={InputField}
-        label='Image url'
-        placeholder='Paste a link to your image here'
-      />
-      <Field name='description' component={TextAreaField}
-        label='Description'
-        placeholder='Description'
-      />
-      <Field name='slack_channel_name' component={TextAreaField}
-        label='Slack channel name'
-        placeholder='slack_channel_name'
-      />
-      <Field name='status'
-        component={SelectField}
-        label='Status'
-        options={options}
-        placeholder='Status'
-      />
-      <FieldArray
-        name='repos'
-        type='repo'
-        start='GitHub url'
-        component={FieldGroup}
-      />
-      <FieldArray
-        name='trackers'
-        type='tracker'
-        start='Issue Tracker'
-        component={FieldGroup} />
-      <Form.Field control={Button} primary
-        type='submit' disabled={submitting}>
-        Create Project
-      </Form.Field>
+      <Grid stackable columns={2}>
+        <Grid.Row>
+          <Grid.Column>
+            <Field name='title' component={InputField} label='Title' placeholder='Title' />
+            <Field
+              type='url'
+              name='image_url'
+              component={InputField}
+              label='Image url'
+              placeholder='Paste a link to your image here'
+            />
+            <Field
+              name='description'
+              component={TextAreaField}
+              label='Description'
+              placeholder='Description'
+            />
+          </Grid.Column>
+          <Grid.Column>
+            <Field
+              name='slack_channel_name'
+              component={TextAreaField}
+              label='Slack channel name'
+              placeholder='slack_channel_name'
+            />
+            <Field
+              name='status'
+              component={SelectField}
+              label='Status'
+              options={options}
+              placeholder='Status'
+            />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <FieldArray
+            name='repos'
+            type='repo'
+            start='GitHub url'
+            component={FieldGroup}
+          />
+          <FieldArray
+            name='trackers'
+            type='tracker'
+            start='Issue Tracker'
+            component={FieldGroup}
+          />
+        </Grid.Row>
+        <Form.Field control={Button} primary type='submit' disabled={submitting}>Create Project
+        </Form.Field>
+      </Grid>
     </Form>
   )
 }
