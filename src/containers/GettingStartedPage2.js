58706133acc626e3439b6e78a0307c88a57510a9
dorotanowak1 <dorotanowak@rocketmail.com>
2019-07-01 23:28:16 +0100

    test added

--- src/containers/GettingStartedPage2.js
+++ src/containers/GettingStartedPage2.js
@@ -4,25 +4,25 @@ import { Container } from 'semantic-ui-react'
 import axios from 'axios'
 import ReactHtmlParser from 'react-html-parser'
 
-export default class GettingStartedPage extends Component {
-  state = { gettingStartedPage: null };
+export default class GettingStartedPage2 extends Component {
+    state = { gettingStartedPage: null };
 
-  componentDidMount () {
-    axios.get('api/v1/static-pages/getting-started-2').then(response => {
-      this.setState({ gettingStartedPage: response.data })
-    })
-  }
-
-  render () {
-    let { gettingStartedPage } = this.state
-    if (gettingStartedPage) {
-      return (
-        <Container>
-          <div>{ReactHtmlParser(gettingStartedPage)}</div>
-        </Container>
-      )
-    } else {
-      return <CustomRingLoader />
+    componentDidMount () {
+      axios.get('api/v1/static-pages/getting-started-2').then(response => {
+        this.setState({ gettingStartedPage: response.data })
+      })
     }
-  }
+
+    render () {
+      let { gettingStartedPage } = this.state
+      if (gettingStartedPage) {
+        return (
+          <Container>
+            <div>{ReactHtmlParser(gettingStartedPage)}</div>
+          </Container>
+        )
+      } else {
+        return <CustomRingLoader />
+      }
+    }
 }
