cb52a4ae77975c0373b749b00d6fb233825728e1
aonomike <aonomike@gmail.com>
2020-05-14 21:20:05 +0300

    Events Response body update
    - Server response body format for a list of events has been changed.
    
    Co-authored-by: James Dindi <dindijjames@gmail.com>
    Co-authored-by: Riccardo Giomi <contact@riccardogiomi.com>
    Co-authored-by: Gideon Kimutai <gideon.kimutai.kim@gmail.com>

--- src/actions/getEventsAction.js
+++ src/actions/getEventsAction.js
@@ -5,7 +5,7 @@ export let fetchEvents = () => dispatch => {
   return axios
     .get('/events.json')
     .then(response => {
-      let events = response.data.map(event => {
+      let events = response.data.events.map(event => {
         event.start = new Date(event.start)
         event.end = new Date(event.end)
         return event
